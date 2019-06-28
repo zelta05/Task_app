3261bb0590280814a6921a4c87fdafbfd335ae44
Liz <Ltriantafelow@gmail.com>
2019-06-28 05:44:56 +0000

    CSS

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,7 +13,23 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get contact_path
     assert_select "title", full_title("Contact")
+
+    @user = users(:archer)
+    log_in_as(@user)
     
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+
+    get users_path
+    assert_select "title", full_title("All users")
+
     get signup_path
     assert_select "title", full_title("Sign up")
   end
